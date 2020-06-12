e03c6ab3e6b56c62ce8118b65cb2b69c145e1dcd
Venkat <raghul.v@gmail.com>
2020-06-12 19:46:02 +0530

    codefor review

---
+++ script.js
@@ -0,0 +1,25 @@
+// Step 1: Validate login from the data.json file without loading the page
+// Step 2: Insert the message from the user in the data.json file after
+// finding the right user if validation is successful
+// Step 3: Hide elements with classname called login-box
+//(use !important as multiple classes would apply to some elements)
+// Step 4: Select the element with id "success-message" visible and then display
+// the user name and message in the selected element
+// Step 5: Select all elements with class rating icon and make it visible
+// The selection looks like this
+{
+  /* <span id="star1" class="rating-icon fa fa-star"></span>
+<span id="star2" class="rating-icon fa fa-star"></span>
+<span id="star3" class="rating-icon fa fa-star"></span>
+<span id="star4" class="rating-icon fa fa-star"></span>
+<span id="star5" class="rating-icon fa fa-star"></span> */
+}
+// Step 6: Setup event listeners on all these star icons so onclick
+// we should be able to change the color of the clicked star
+// and all the previous stars to #FE9A27
+// Step 7: Final step where you save the rating against the user in the data.json file
+// IMPORTANT: THE PAGE NEVER RELOADS IN ALL THIS :)
+
+//Step 1
+
+
